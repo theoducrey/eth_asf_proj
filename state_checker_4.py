277df669a8b0f38e2191a40a158aa7f9994d1959
Fedor M <37484405+FedorMi@users.noreply.github.com>
2024-04-22 19:08:25 +0200

    Update state_checker_4.py
    states comparison, using states as collection of edges and comparing these edges

--- state_checker_4.py
+++ state_checker_4.py
@@ -15,8 +15,21 @@ class StateChecker:
 
     def process_state(self, state):
         #TODO compare state with past states
+        differences = [] # each list in this list corresponds to differences between states for each saved state
 
+        for i in self.state_accumulator:
+            differences = self.compare_states(state, i)
 
         self.state_accumulator.append(state)
 
         pass
+
+    def compare_states(self, state1, state2):
+        both_have = [] # both have these edges
+        only_first = [] # only first state has these edges 
+        for i in state1:
+            if i in state2:
+                both_have.append(i)
+            else:
+                only_first.append(i)
+        return only_first
