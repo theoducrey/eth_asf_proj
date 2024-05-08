a6791c766c7699c2694ea19baed985925b526c79
Fedor M <37484405+FedorMi@users.noreply.github.com>
2024-05-08 17:30:52 +0200

    checker for "create -> open" have "B"
    checker for "create -> open" have "B" in the manifest graph

--- trace_analyzer_5.py
+++ trace_analyzer_5.py
@@ -1,3 +1,4 @@
+from manifestGraph import ManifestGraph
 class TraceAnalyzer:
     def __init__(self, logger, queue_basic_block_trace, main_lock, manifest_graph, args):
         self.main_lock = main_lock
@@ -17,7 +18,19 @@ class TraceAnalyzer:
             block_trace = self.queue_basic_block_trace.get()  # every mutation is a sequence of operation to be applied together before running the puppet manifest on the fresh image
             self.process_block_trace(block_trace)
 
-    def process_block_trace(self, state):
-        self.manifest_graph.ver
+    def process_block_trace(self, mgraph: ManifestGraph, trace):
         #Directy log the result using the result logger
-        pass
+        #TODO
+        # create -> open check
+        relations = {}
+        for i in trace :
+            for j in i['new']:
+                for k in trace:
+                    if k != i and j in k['open']:
+                        relations[i][k] = 0
+        missing_dependencies = []
+        for i in relations:
+            for j in relations[i]:
+                if mgraph.edge_res1_res2(i, j) != "B":
+                    missing_dependencies.append([i, j])
+        return missing_dependencies
